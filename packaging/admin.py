b3eceb251c8ee7965a20f2de411545a81daddc77
Adiom Timur <adiom@canfly.org>
2024-03-25 13:25:49 +0300

    Добавил админа

--- packaging/admin.py
+++ packaging/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Shipment, Product, Seller
+
+admin.site.register(Shipment)
+admin.site.register(Product)
+admin.site.register(Seller)
+
+class ShipmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'product', 'seller', 'shipment_date', 'status')
+    list_filter = ('product', 'seller', 'status')
+
+admin.site.register(Shipment, ShipmentAdmin)
--- packaging/models.py
+++ packaging/models.py
@@ -1,66 +1,25 @@
 from django.db import models
-from django.shortcuts import render
-from .models import Client
 
+# Model "Shipments"
+class Shipment(models.Model):
+    main_sticker = models.CharField(max_length=255)
+    small_sticker = models.CharField(max_length=255)
+    code128 = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    seller = models.ForeignKey('Seller', on_delete=models.CASCADE)
+    shipment_date = models.DateField()
+    status = models.CharField(max_length=255, choices=[('New', 'New'), ('In progress', 'In progress'), ('Shipped', 'Shipped')])
 
-# ---------тут ничего не сделано, я примерно прикинул какие поля как будут выглядеть и функционировать поля нашей модели--------
+# Model "Products"
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    sku = models.CharField(max_length=255)
+    quantity_in_stock = models.IntegerField()
+    quantity_at_fbo = models.IntegerField()
 
-class Client(models.Model):
-    name = models.CharField(max_length=100, help_text='Имя')
-    surname = models.CharField(max_length=100,help_text='Фамилия')
-    email = models.EmailField(max_length= 100,help_text='Адрес электронной почты')
-    adres = models.CharField(max_length=512, help_text='Адрес')
-    inn = models.CharField(max_length=12, help_text='ИНН')
-    bank_account = models.FileField(help_text='Расчетный счет')
-    BIK = models.CharField(max_lenght=9, help_text='БИК')
-    contract = models.FileField(help_text='Договор')
-
-    # Другие поля, такие как email, password и т.д.
-    # Метаданные
-    class Meta:
-        ordering = ['-my_field_name']
-
-    # Methods
-    def get_absolute_url(self):
-        """Возвращает URL-адрес для доступа к определенному экземпляру MyModelName."""
-        return reverse('model-detail-view', args=[str(self.id)])
-
-    def __str__(self):
-        """Строка для представления объекта MyModelName (например, в административной панели и т.д.)."""
-        return self.my_field_name
-# views.py
-
-
-def client_dashboard(request, client_id):
-    client = Client.objects.get(id=client_id)
-    # Получение данных о товарах клиента и т.д.
-    return render(request, 'client_dashboard.html', {'client': client})
-
-
-from django.db import models
-from django.urls import reverse
-
-
-
-
-
-# -----------не обращай внимания на код ниже---------------------
-# class MyModelName(models.Model):
-#     """Типичный класс модели, производный от класса Model."""
-
-#     # Поля
-#     my_field_name = models.CharField(max_length=20, help_text='Введите описание поля')
-#     # …
-
-#     # Метаданные
-#     class Meta:
-#         ordering = ['-my_field_name']
-
-#     # Methods
-#     def get_absolute_url(self):
-#         """Возвращает URL-адрес для доступа к определенному экземпляру MyModelName."""
-#         return reverse('model-detail-view', args=[str(self.id)])
-
-#     def __str__(self):
-#         """Строка для представления объекта MyModelName (например, в административной панели и т.д.)."""
-#         return self.my_field_name
+# Model "Sellers"
+class Seller(models.Model):
+    name = models.CharField(max_length=255)
+    tax_id = models.CharField(max_length=255)
+    email = models.EmailField()
