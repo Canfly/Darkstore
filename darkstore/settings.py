6cf8e824b386d279253b7d5d733429208132e3a6
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2024-03-25 17:20:46 +0300

    some settings for django-channles

--- darkstore/settings.py
+++ darkstore/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'packaging.apps.PackagingConfig',
-    'django_celery_beat'
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -68,6 +68,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'darkstore.wsgi.application'
+ASGI_APPLICATION = 'darkstore.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
